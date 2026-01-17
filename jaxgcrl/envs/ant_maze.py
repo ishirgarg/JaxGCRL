2ccd4f9319fa62fa9209957a17ec6713bff3b298
Ishir Garg <gargishir@gmail.com>
2026-01-17 01:09:05 -0800

    Add larger mazes

--- jaxgcrl/envs/ant_maze.py
+++ jaxgcrl/envs/ant_maze.py
@@ -78,6 +78,58 @@ BIG_MAZE_ONE_CORNER = [
     [1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+BIGGER_MAZE_THREE_CORNER = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, R, S, S, 1, 1, S, S, S, 1, 1, S, G, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, S, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, S, S, S, S, 1, S, 1],
+    [1, 1, 1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, 1, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, G, S, S, S, S, S, 1, S, S, S, S, G, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+BIGGER_MAZE_TWO_CORNER = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, R, S, S, 1, 1, S, S, S, 1, 1, S, G, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, S, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, S, S, S, S, 1, S, 1],
+    [1, 1, 1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, 1, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, G, S, S, S, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+BIGGER_MAZE_ONE_CORNER = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, R, S, S, 1, 1, S, S, S, 1, 1, S, S, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, S, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, S, S, S, S, 1, S, 1],
+    [1, 1, 1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1],
+    [1, S, S, S, S, S, S, S, S, 1, S, S, S, 1],
+    [1, S, 1, 1, 1, S, 1, 1, S, 1, 1, 1, S, 1],
+    [1, S, S, S, 1, S, S, 1, S, S, S, S, S, 1],
+    [1, 1, 1, S, 1, 1, 1, 1, 1, 1, S, 1, 1, 1],
+    [1, G, S, S, S, S, S, 1, S, S, S, S, G, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+
 
 BIG_MAZE_EVAL = [
     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -140,6 +192,12 @@ def make_maze(maze_layout_name, maze_size_scaling):
         maze_layout = BIG_MAZE_TWO_CORNER
     elif maze_layout_name == "big_maze_three_corner":
         maze_layout = BIG_MAZE_THREE_CORNER
+    elif maze_layout_name == "bigger_maze_one_corner":
+        maze_layout = BIGGER_MAZE_ONE_CORNER
+    elif maze_layout_name == "bigger_maze_two_corner":
+        maze_layout = BIGGER_MAZE_TWO_CORNER
+    elif maze_layout_name == "bigger_maze_three_corner":
+        maze_layout = BIGGER_MAZE_THREE_CORNER
     elif maze_layout_name == "big_maze_eval":
         maze_layout = BIG_MAZE_EVAL
     elif maze_layout_name == "hardest_maze":
